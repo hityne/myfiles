2dab2bfa01f7d5c6c4dbfbbb94163eaac5a0e7ec
hityne <long@live.ca>
2022-03-25 22:35:11 +0800

    添加删除功能

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path, re_path, include
 from . import views
+from django.views.static import serve
+from myfiles import settings
 
 urlpatterns = [
     #path('', views.index),
+    re_path(r'^del', views.delete),
+    re_path(r'^(?P<path>.+\.jpg)', serve, {"document_root":settings.REPO_ROOT}),
+    re_path(r'^(?P<path>.+\.jpeg)', serve, {"document_root":settings.REPO_ROOT}),
+    re_path(r'^(?P<path>.+\.gif)', serve, {"document_root":settings.REPO_ROOT}),
+    re_path(r'^(?P<path>.+\.png)', serve, {"document_root":settings.REPO_ROOT}),
+    re_path(r'^(?P<path>.+\.mp4)', serve, {"document_root":settings.REPO_ROOT}),
     re_path(r'^(.*)$', views.deep),
+
+
 ]
+
+
--- index/views.py
+++ index/views.py
@@ -1,7 +1,7 @@
 import json
 import os
 import time
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponseRedirect
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 # Create your views here.
@@ -18,17 +18,41 @@ def deep(request, name):
     repo_path = './myrepo'
     new_path = os.path.join(repo_path, name)
     if os.path.isfile(new_path):
+        if new_path.endswith('.jpg'):
+            return render(request, 'img.html', {'data': {'name': name}})
         # 读取文件
-        file = open(new_path, 'rb')
-        response = FileResponse(file)
+        else:
+            file = open(new_path, 'rb')
+            response = FileResponse(file)
 
-        # 使用urlquote对文件名称进行编码
-        name = name[name.rfind('/') + 1:]
-        response['Content-Disposition'] = 'attachment;filename="%s"' % urlquote(name)
+            # 使用urlquote对文件名称进行编码
+            name = name[name.rfind('/') + 1:]
+            response['Content-Disposition'] = 'attachment;filename="%s"' % urlquote(name)
 
-        return response
+            return response
     elif os.path.isdir(new_path):
         data = get_data(repo_path, name)
         return render(request, 'index.html', {'data': data})
     else:
         return HttpResponse('查无此文件')
+
+
+def img(request):
+    # data = get_data('./myrepo', '')
+    return render(request, 'img.html', locals())
+
+
+def delete(request):
+    a = request.GET.get('a')
+    print(a)
+    b = request.GET.get('b')
+
+    print(b)
+    repo_path = './myrepo'
+    new_path = os.path.join(repo_path, a, b)
+    if os.path.exists(new_path):
+        print(new_path)
+        os.remove(new_path)
+        return HttpResponseRedirect('/'+a)
+    else:
+        return HttpResponse("没有此文件！")
