b515154f64931ba56c86d28d230ec0ce6a0cacb8
hityne <long@live.ca>
2022-03-26 07:42:13 +0800

    修复多层级时路径错误

--- mycodes/func.py
+++ mycodes/func.py
@@ -2,9 +2,9 @@ import os
 import time
 
 
-def get_data(repo_path, name):
-    path = os.path.join(repo_path, name)
-    data = {'dirs': [], 'files': [], 'path': name.split('/')}
+def get_data(repo_path, file_path):
+    path = os.path.join(repo_path, file_path)
+    data = {'dirs': [], 'files': [], 'path': file_path.split('/')}
     for each in os.listdir(path):
         if each.startswith('.') is False and each.startswith('~') is False:
             mypath = os.path.join(path, each)
@@ -12,12 +12,12 @@ def get_data(repo_path, name):
                 myname = each
                 mytime = time.strftime('%y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(mypath)))
                 mysize = '-'
-                data['dirs'].append({'name': myname, 'time': mytime, 'size': mysize, 'path': name})
+                data['dirs'].append({'name': myname, 'time': mytime, 'size': mysize, 'path': file_path})
             elif os.path.isfile(mypath):
                 myname = each
                 mytime = time.strftime('%y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(mypath)))
                 mysize = str(round(os.path.getsize(mypath) / 1024)) + ' KB'
-                data['files'].append({'name': myname, 'time': mytime, 'size': mysize, 'path': name})
+                data['files'].append({'name': myname, 'time': mytime, 'size': mysize, 'path': file_path})
 
     return data
 
