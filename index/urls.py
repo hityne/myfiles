5704eca0ff3f558c801f7d4d57759f99cfc50b50
Richard <accdou@163.com>
2022-03-28 15:18:36 +0800

    修复视频不能快进问题

--- index/urls.py
+++ index/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     # re_path(r'^(?P<path>.+\.png)', serve, {"document_root":settings.REPO_ROOT}),
     # re_path(r'^(?P<path>.+\.pdf)', serve, {"document_root":settings.REPO_ROOT}),
     # re_path(r'^(?P<path>.+\.txt)', serve, {"document_root":settings.REPO_ROOT}),
-    # re_path(r'^(?P<path>.+\.mp4)$', views.stream_video),
-    # re_path(r'^(?P<path>.+\.mkv)$', views.stream_video),
+    re_path(r'^(?P<path>.+\.mp4)$', views.stream_video),
+    re_path(r'^(?P<path>.+\.mkv)$', views.stream_video),
     re_path(r'^(?P<path>.+\..+)$', serve, {"document_root": settings.REPO_ROOT}),
     re_path(r'^(.*)$', views.deep),
 
